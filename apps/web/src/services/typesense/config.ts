89b143051b7165db20a4a9f537e81fee8c0ad435
Diogo Silva <diogouzu@gmail.com>
2025-11-05 16:37:49 -0300

    Organizacão packages

--- apps/web/src/services/typesense/config.ts
+++ apps/web/src/services/typesense/config.ts
@@ -1,11 +1,14 @@
+import type { ConfigurationOptions } from "typesense/lib/Typesense/Configuration"
+
 export function assembleTypesenseServerConfig() {
-  const TYPESENSE_SERVER_CONFIG = {
+  const TYPESENSE_SERVER_CONFIG: ConfigurationOptions = {
     apiKey: process.env.NEXT_PUBLIC_TYPESENSE_SEARCH_KEY!,
     nodes: [
       {
         host: process.env.NEXT_PUBLIC_TYPESENSE_HOST!,
         port: Number(process.env.NEXT_PUBLIC_TYPESENSE_PORT),
-        protocol: 'https'
+        protocol: 'https',
+        path: '/typesense'
       }
     ],
     numRetries: 8,
