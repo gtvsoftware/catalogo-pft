302441bc19daea904c09a2fb21901bb64fce6baf
Diogo Silva <diogouzu@gmail.com>
2025-11-11 20:48:34 -0300

    Organizacão packages

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,8 +1,12 @@
 import { generateMiddleware } from '@terraviva/auth'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function proxy(req: NextRequest) {
   const { middlewareFn } = await generateMiddleware()
+
+  if (process.env.NEXT_PUBLIC_DISABLE_AUTH) {
+    return NextResponse.next()
+  }
 
   return middlewareFn(req as any)
 }
